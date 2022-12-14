247f9bd5567a6f0f147bcfb424e00da32b4c33c1
athiraanil11 <athiraanil093@gmail.com>
2022-12-14 21:52:17 +0530

    completed

---
+++ src/app/addstatewise/addstatewise.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addstatewise',
+  templateUrl: './addstatewise.component.html',
+  styleUrls: ['./addstatewise.component.css']
+})
+export class AddstatewiseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddstatewiseComponent } from './addstatewise/addstatewise.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ViewallComponent } from './viewall/viewall.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+
+const myroutes=[
+
+  {path:"",component:AddstatewiseComponent},
+  {path:"view",component:ViewallComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddstatewiseComponent,
+    NavbarComponent,
+    ViewallComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myroutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
